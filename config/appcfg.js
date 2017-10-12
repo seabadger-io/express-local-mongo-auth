24d8c51db3c05414e5f8bd073c8f1ea4be4fdd4c
Gabor Szoboszlai <itgipsy@itgipsy.com>
2017-10-12 18:07:09 +0000

    Added HTTPS support

--- config/appcfg.js
+++ config/appcfg.js
@@ -1,5 +1,15 @@
 module.exports = {
   db: {
     url: 'mongodb://localhost/multi-auth'
+  },
+  http: {
+    enabled: true,
+    port: 8080
+  },
+  https: {
+    enabled: true,
+    port: 8443,
+    crt: 'config/cert/server.crt',
+    key: 'config/cert/server.key'
   }
 };
